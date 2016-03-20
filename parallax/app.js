93864534d8430d89ea0f88699c56e652f00278e1
93lucasp <spezzanoluca@gmail.com>
2016-03-20 12:23:48 -0700

    effect parallax

---
+++ parallax/app.js
@@ -0,0 +1,13 @@
+  $(document).ready(function() {
+  	parallax();
+  });
+
+ function parallax() {
+  		var documentEl = $(document),
+          parallaxBg = $('div.parallax-bg');
+          
+      documentEl.on('scroll', function() {
+        var currScrollPos = documentEl.scrollTop();
+        parallaxBg.css('background-position', '0' + -currScrollPos/2 + 'px');
+      });
+    }
